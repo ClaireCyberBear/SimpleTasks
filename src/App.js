50b68e43c5f49fc0697ccc130cd0216b2f4caee4
ClaireCyberBear <clairecyberbear@gmail.com>
2023-05-11 11:44:51 -0500

    Created a componentfolder to import to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import supabase from "./supabase";
 import "./style.css";
+import { Menu } from "./components";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false); //Menu sidebar by default is closed.
@@ -32,7 +33,7 @@ function App() {
         <button className="btn btn-login hidden">Login</button>
       </header>
       <div className="menu-task">
-        <MenuButton
+        <Menu
           showMenu={showMenu}
           setShowMenu={setShowMenu}
           setShowNewTask={setShowNewTask}
@@ -93,51 +94,6 @@ function NewTask({ setTasks, setShowNewTask }) {
         <button className="btn form-btn">Add</button>
       </div>
     </form>
-  );
-}
-
-//Little function for the MenuButton, when clicked it sets showMenu to true
-function MenuButton({ showMenu, setShowMenu, setShowNewTask }) {
-  return (
-    <aside className="sidebar">
-      <button
-        className="btn sidebar-open"
-        onClick={() => setShowMenu(!showMenu)}
-      >
-        {showMenu ? "Close" : "Menu"}
-      </button>
-      {showMenu ? (
-        <SideMenu setShowNewTask={setShowNewTask} setShowMenu={setShowMenu} />
-      ) : null}
-    </aside>
-  );
-}
-
-//Html for the side bar, this is hidden until the showMenu state becomes true
-function SideMenu({ setShowNewTask, setShowMenu }) {
-  const handleClick = () => {
-    setShowNewTask(true);
-    setShowMenu(false);
-  };
-  return (
-    <ul className="btn-sidebar">
-      <li>
-        <button className="btn">
-          <a href="https://github.com/ClaireCyberBear/SimpleTasks">
-            Source Code
-          </a>
-        </button>
-      </li>
-      <li>
-        <button className="btn tasklist-btn">Task List</button>
-      </li>
-
-      <li className="newtask">
-        <button className="btn" onClick={handleClick}>
-          New Task
-        </button>
-      </li>
-    </ul>
   );
 }
 
