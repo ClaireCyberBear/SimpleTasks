41da4a4a1a2bd0d1f07df6f5f58b4b10739b01ba
ClaireCyberBear <clairecyberbear@gmail.com>
2023-05-11 14:25:14 -0500

    Imported react into header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 export function Header() {
   return (
     <header className="header">
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 export function Menu({ showMenu, setShowMenu, setShowNewTask }) {
   return (
     <MenuButton
