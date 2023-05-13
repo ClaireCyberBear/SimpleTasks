505b373c3450deee9391fd6a25d5b039a8a904a6
ClaireCyberBear <clairecyberbear@gmail.com>
2023-05-13 15:17:06 -0500

    Setting up asp project

---
+++ src/server/Models/User.cs
@@ -0,0 +1,8 @@
+namespace Models{
+
+public class User {
+    public int Id {get; set;}
+    public string? Username {get; set;}
+    public int Pin {get; set;}
+    public bool IsLoggedIn {get; set;}
+}}
--- src/server/Program.cs
+++ src/server/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+string? myConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(myConnectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
